4477bcef176872c6fbb9527906b9156d09dc8169
Dhairya <dhairya00798@gmail.com>
2020-04-04 12:56:36 +0530

    added inline css

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -16,7 +16,9 @@ class Counter extends Component {
         <span style={this.styles} className="badge badge-primary m-2">
           {this.formatCount()}
         </span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <button style={{ fontSize: 20 }} className="btn btn-secondary btn-sm">
+          Increment
+        </button>
       </div>
     );
   }
